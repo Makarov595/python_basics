7725afb39820944b69cea6713222dffe62dc7a23
Makarov595 <makarov_595@mail.ru>
2020-04-04 13:12:13 +0500

    Homework lesson_3

---
+++ 3/3_les_2_exercise.py
@@ -0,0 +1,6 @@
+def info(name, surname, year_of_birth, current_city, e_mail, phone_number):
+    print(f'Имя: {name}, Фамилия: {surname}, год рождения: {year_of_birth}, '
+          f'город проживания: {current_city}, e-mail: {e_mail}, телефон: {phone_number}')
+
+
+info('Иванов', 'Иван', 1992, 'Москва', 'IIvanov@mail.ru', 89258888888)
